90b8bf03843206c906f370d7a3e9c1b97bcd319b
KEERTHANA P <161140175+keerthanapnair@users.noreply.github.com>
2024-02-26 19:54:21 +0530

    Add files via upload

---
+++ Smallest.py
@@ -0,0 +1,10 @@
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+c=int(input("enter c value:"))
+if (a<b) and (a<c):
+    print("smallest = a")
+elif ( b<a) and (b<c):
+    print("smallest = b")
+else:
+    print("smallest = c")
+    
